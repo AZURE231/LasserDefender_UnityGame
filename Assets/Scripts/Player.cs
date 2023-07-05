e00ef44e44dd441e3fed50be611ea64b9a75cbc9
AZURE231 <95262095+AZURE231@users.noreply.github.com>
2023-07-05 16:54:51 +0700

    Boundaries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,16 +6,35 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     Vector2 rawInput;
+    Vector2 minBounds;
+    Vector2 maxBounds;
     [SerializeField] float playerSpeed = 1f;
+    [SerializeField] float padding = 5f;
+
+    private void Start()
+    {
+        InitBounds();
+    }
+
     void Update()
     {
         Move();
     }
 
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
     private void Move()
     {
-        Vector3 delta = rawInput;
-        transform.position += delta * Time.deltaTime * playerSpeed;
+        Vector2 delta = rawInput * Time.deltaTime * playerSpeed;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + padding, maxBounds.x - padding);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + padding, maxBounds.y - padding);
+        transform.position = newPos;
     }
 
     void OnMove(InputValue value)
