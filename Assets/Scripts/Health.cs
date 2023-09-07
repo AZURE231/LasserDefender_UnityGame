d5e532f29d26855a34c5634a9d852b0c3853cd61
AZURE231 <95262095+AZURE231@users.noreply.github.com>
2023-09-07 21:53:02 +0700

    Camera shake

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,14 @@ public class Health : MonoBehaviour
 {
     [SerializeField] int health = 50;
     [SerializeField] ParticleSystem hitEffect;
+
+    [SerializeField] bool applyCameraShake;
+    CameraShake cameraShake;
+
+    private void Awake()
+    {
+        cameraShake = Camera.main.GetComponent<CameraShake>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -16,7 +25,16 @@ public class Health : MonoBehaviour
             // Take damage.
             TakeDamage(damageDealer.GetDamage());
             PlayHitEffect();
+            ShakeCamera();
             damageDealer.Hit();
+        }
+    }
+
+    private void ShakeCamera()
+    {
+        if (cameraShake != null && applyCameraShake)
+        {
+            cameraShake.Play();
         }
     }
 
