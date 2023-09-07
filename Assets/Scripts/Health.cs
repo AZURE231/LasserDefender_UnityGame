327b92515151f08ab0a6f283f88c7ada1d7b83df
AZURE231 <95262095+AZURE231@users.noreply.github.com>
2023-09-07 14:35:29 +0700

    particle effect on hit

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] int health = 50;
+    [SerializeField] ParticleSystem hitEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -13,8 +14,9 @@ public class Health : MonoBehaviour
         if (damageDealer)
         {
             // Take damage.
-            damageDealer.Hit();
             TakeDamage(damageDealer.GetDamage());
+            PlayHitEffect();
+            damageDealer.Hit();
         }
     }
 
@@ -25,6 +27,15 @@ public class Health : MonoBehaviour
         if (health <= 0)
         {
             Destroy(gameObject);
+        }
+    }
+
+    void PlayHitEffect()
+    {
+        if (hitEffect != null)
+        {
+            ParticleSystem instance = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax);
         }
     }
 }
